ff8dcdc266c2c5c18bb1cfff90239771213a458d
Ethan Croft <ethan.croft@natwest.com>
2023-09-26 09:33:27 +0100

    Install and begin to configure inView to trigger lazy loading

--- src/components/Search/Results/Tracks.jsx
+++ src/components/Search/Results/Tracks.jsx
@@ -1,46 +1,73 @@
+import { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Fragment } from "react";
+
+import { useInView } from "react-intersection-observer";
 
 export const Tracks = (props) => {
+    const { inView: trackInView, ref } = useInView({
+        threshold: 0,
+    });
+
+    useEffect(() => {
+        if (props.filterIsSelected) {
+            console.log(trackInView);
+        }
+    }, [trackInView, props.filterIsSelected]);
+
     if (props.tracks.items.length !== 0) {
         return (
             <Fragment>
                 {props.filterIsSelected === false && <h2>Tracks</h2>}
 
-                <ul>
+                {
+                    trackInView && props.filterIsSelected
+                    // && props.onLoadMore({
+                    //     pageParam: props.tracks.next,
+                    // })
+                }
+                <ul ref={ref}>
                     {props.tracks.items.map((track) => {
                         const duration = new Date(track.duration_ms)
                             .toISOString()
                             .slice(11, 19);
 
                         return (
-                            <li key={track.id}>
-                                <div>
-                                    {
-                                        <img
-                                            src={track.album.images[2].url}
-                                            alt={`Cover art for album: ${track.album.name}`}
-                                        />
-                                    }
-                                    {track.name}
-                                    {track.explicit ? <span>E</span> : ""}
-                                    {track.artists.map(
-                                        (artist, index) =>
-                                            `${artist.name}${
-                                                index < track.artists.length - 1
-                                                    ? ", "
-                                                    : ""
-                                            }`,
-                                    )}
-                                    {props.filterIsSelected && track.album.name}
-                                    {duration.startsWith("00:")
-                                        ? duration.slice(3)
-                                        : duration}
-                                </div>
-                            </li>
+                            <Fragment key={track.id}>
+                                <li>
+                                    <div>
+                                        {
+                                            <img
+                                                src={track.album.images[2].url}
+                                                alt={`Cover art for album: ${track.album.name}`}
+                                            />
+                                        }
+                                        {track.name}
+                                        {track.explicit ? <span>E</span> : ""}
+                                        {track.artists.map(
+                                            (artist, index) =>
+                                                `${artist.name}${
+                                                    index <
+                                                    track.artists.length - 1
+                                                        ? ", "
+                                                        : ""
+                                                }`,
+                                        )}
+                                        {props.filterIsSelected &&
+                                            track.album.name}
+                                        {duration.startsWith("00:")
+                                            ? duration.slice(3)
+                                            : duration}
+                                    </div>
+                                </li>
+                            </Fragment>
                         );
                     })}
                 </ul>
+                {props.filterIsSelected && (
+                    <button onClick={props.onLoadMore(props.tracks.next)}>
+                        More
+                    </button>
+                )}
             </Fragment>
         );
     } else {
@@ -51,4 +78,5 @@ export const Tracks = (props) => {
 Tracks.propTypes = {
     tracks: PropTypes.object.isRequired,
     filterIsSelected: PropTypes.bool.isRequired,
+    onLoadMore: PropTypes.func.isRequired,
 };
