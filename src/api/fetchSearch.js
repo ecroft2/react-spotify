a1a59f100fff6e2fb3a5a57b426b6a0f2cde8654
Ethan Croft <ethan.croft@natwest.com>
2023-09-26 09:28:05 +0100

    Remove query string logic out of fetchSearch to follow React Query useInfiniteQuery convention

--- src/api/fetchSearch.js
+++ src/api/fetchSearch.js
@@ -1,9 +1,5 @@
-export const fetchSearch = async ({ queryKey }) => {
-    const base = "https://api.spotify.com/v1/search";
-
-    const queryString = `?q=${queryKey[1].searchQuery}&type=${queryKey[1].filter === "all" ? "album,artist,playlist,track,show,episode,audiobook" : queryKey[1].filter}${queryKey[1].limit ? `&limit=${queryKey[1].limit}` : ""}`;
-
-    const apiResponse = await fetch(base + queryString, {
+export const fetchSearch = async ({ queryKey, pageParam = queryKey[1] }) => {
+    const apiResponse = await fetch(pageParam, {
         method: "GET",
         headers: {
             "Authorization": `Bearer ${import.meta.env.VITE_REACT_SPOTIFY_API_TOKEN}`
@@ -11,7 +7,7 @@ export const fetchSearch = async ({ queryKey }) => {
     });
 
     if (!apiResponse.ok) {
-        throw new Error(`Error fetching music from URL ${base + queryString}`);
+        throw new Error(`Error fetching music from URL ${pageParam}`);
     }
 
     return apiResponse.json();
