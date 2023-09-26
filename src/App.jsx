097d7550d8f50a4e3d5f646fb5c529165efd7e38
Ethan Croft <ethan.croft@natwest.com>
2023-09-26 09:26:32 +0100

    Install react query dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, Fragment } from "react";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Search } from "./components/Search/Search";
@@ -24,6 +26,7 @@ const App = () => {
 
                 {searchQuery && <Results searchQuery={searchQuery} />}
             </Fragment>
+            <ReactQueryDevtools initialIsOpen={true} />
         </QueryClientProvider>
     );
 };
